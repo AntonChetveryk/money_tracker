1c5d99b2aa2f4806063832c2375eb9db10c00d80
AntonChetveryk <chetverykanton92@gmail.com>
2020-07-05 22:41:18 +0300

    Updates

---
+++ precache-manifest.cb104c7000da4f2575667bb4c055818e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea1f9868f43ba9eb83949b6f2fe2665",
+    "url": "/money_tracker/index.html"
+  },
+  {
+    "revision": "68a9878ec9a34a26bd3e",
+    "url": "/money_tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "68a9878ec9a34a26bd3e",
+    "url": "/money_tracker/static/js/2.6777d588.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/money_tracker/static/js/2.6777d588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edfd5b3047b4332bc0c9",
+    "url": "/money_tracker/static/js/main.0b88153b.chunk.js"
+  },
+  {
+    "revision": "d0195189364f972c2199",
+    "url": "/money_tracker/static/js/runtime-main.a87f3234.js"
+  }
+]);
